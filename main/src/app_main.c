8fcf372df6275b68e5f954d50055a2174bddbb4a
Mihetov <artyr815@gmail.com>
2026-03-07 02:08:44 +0400

    Fix HTTP server startup by initializing esp_netif/lwIP

--- main/src/app_main.c
+++ main/src/app_main.c
@@ -4,13 +4,22 @@
 #include "esp_check.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_netif.h"
 #include "nvs_flash.h"
 
 static const char *TAG = "app_main";
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
+    esp_err_t nvs_err = nvs_flash_init();
+    if (nvs_err == ESP_ERR_NVS_NO_FREE_PAGES || nvs_err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        nvs_err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(nvs_err);
+
+    // Required for lwIP/tcpip stack used by esp_http_server.
+    ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     ESP_ERROR_CHECK(app_service_init());
